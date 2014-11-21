f27a22de258ae127fd403bd8dfd2553b4ff9858d
Iraklis A. Klampanos <iaklampanos@gmail.com>
2014-11-21 09:56:13 +0000

    .

--- dj_vercereg/dj_vercereg/local_settings_template.py
+++ dj_vercereg/dj_vercereg/local_settings_template.py
@@ -1,4 +1,8 @@
+# local_settings.py 
+# Contains local configuration parameters. local_settings.py is in .gitignore and should not be revision-controlled, as it's site-specific.
+
 DEBUG = True
+ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
